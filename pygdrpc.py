444ec802ef6dd7748a41b2d7020562ed7e40c4ec
Spookybear0 <collinmcarroll@gmail.com>
2020-04-24 13:13:29 -0700

    Make finding difficulty faces faster
    Thanks to nekit for helping on this commit.

--- pygdrpc.py
+++ pygdrpc.py
@@ -1,6 +1,5 @@
 from pypresence import Presence
 import time
-import os
 import gd
 import asyncio
 smallicon = "none"
@@ -36,81 +35,17 @@ async def if_playing_level(id):
             return "Playing an editor level"
     else:
         return "Playing a level."
-async def level_difficulty():
+async def get_diff(level: gd.Level) -> str:
     level = await client.get_level(id)
     if playinglevel == "Playing a level.":
-        if level.is_featured() == True:
-            if str(level.difficulty) == "Auto":
-                smallicon = "auto-featured"
-            if str(level.difficulty) == "Easy":
-                smallicon = "easy-featured"
-            if str(level.difficulty) == "Normal":
-                smallicon = "normal-featured"
-            if str(level.difficulty) == "Hard":
-                smallicon = "hard-featured"
-            if str(level.difficulty) == "Harder":
-                smallicon = "harder-featured"
-            if str(level.difficulty) == "Insane":
-                smallicon = "insane-featured"
-            if str(level.difficulty) == "Easy Demon":
-                smallicon = "demon-easy-featured"
-            if str(level.difficulty) == "Medium Demon":
-                smallicon = "demon-medium-featured"
-            if str(level.difficulty) == "Hard Demon":
-                smallicon = "demon-hard-featured"
-            if str(level.difficulty) == "Insane Demon":
-                smallicon = "demon-insane-featured"
-            if str(level.difficulty) == "Extreme Demon":
-                smallicon = "demon-extreme-featured"
-        if level.is_epic() == True:
-            if str(level.difficulty) == "Auto":
-                smallicon = "auto-epic"
-            if str(level.difficulty) == "Easy":
-                smallicon = "easy-epic"
-            if str(level.difficulty) == "Normal":
-                smallicon = "normal-epic"
-            if str(level.difficulty) == "Hard":
-                smallicon = "hard-epic"
-            if str(level.difficulty) == "Harder":
-                smallicon = "harder-epic"
-            if str(level.difficulty) == "Insane":
-                smallicon = "insane-epic"
-            if str(level.difficulty) == "Easy Demon":
-                smallicon = "demon-easy-epic"
-            if str(level.difficulty) == "Medium Demon":
-                smallicon = "demon-medium-epic"
-            if str(level.difficulty) == "Hard Demon":
-                smallicon = "demon-hard-epic"
-            if str(level.difficulty) == "Insane Demon":
-                smallicon = "demon-insane-epic"
-            if str(level.difficulty) == "Extreme Demon":
-                smallicon = "demon-extreme-epic"
-        if level.is_epic() == False and level.is_featured() == False:
-            if str(level.difficulty) == "Auto":
-                smallicon = "auto"
-            if str(level.difficulty) == "Easy":
-                smallicon = "easy"
-            if str(level.difficulty) == "Normal":
-                smallicon = "normal"
-            if str(level.difficulty) == "Hard":
-                smallicon = "hard"
-            if str(level.difficulty) == "Harder":
-                smallicon = "harder"
-            if str(level.difficulty) == "Insane":
-                smallicon = "insane"
-            if str(level.difficulty) == "Easy Demon":
-                smallicon = "demon-easy"
-            if str(level.difficulty) == "Medium Demon":
-                smallicon = "demon-medium"
-            if str(level.difficulty) == "Hard Demon":
-                smallicon = "demon-hard"
-            if str(level.difficulty) == "Insane Demon":
-                smallicon = "demon-insane"
-            if str(level.difficulty) == "Extreme Demon":
-                smallicon = "demon-extreme"
-        if str(level.difficulty) == "NA":
-                smallicon = "unrated"
-        return smallicon
+        base = level.difficulty.name.lower().split('_')
+        if level.is_epic():
+            base.append('epic')
+        elif level.is_featured():
+            base.append('featured')
+        elif level.is_rated():
+            base.append('rated')
+    return '-'.join(base)
 while True:
     memory.reload()
     id = memory.get_level_id()
@@ -121,5 +56,5 @@ while True:
     else:
         percent = "(" + str(percent) + "%)"
     name = asyncio.run(get_level_from_id(id))
-    RPC.update(pid=memory.process_id, state=str(f"{name} {percent}"), details=playinglevel, large_image="gd", small_image=asyncio.run(level_difficulty()))
+    RPC.update(pid=memory.process_id, state=str(f"{name} {percent}"), details=playinglevel, large_image="gd", small_image=asyncio.run(get_diff(id)))
     time.sleep(10)
