86322fbcf417055e482a9f37a5b46626157f25d5
b1tt <46069798+bitt0008@users.noreply.github.com>
2020-04-27 22:05:36 +0300

    Update pygdrpc.py

--- pygdrpc.py
+++ pygdrpc.py
@@ -3,13 +3,15 @@ import time
 import gd
 import asyncio
 import os
-version = "1.1.1"
-print(f"Version is {version}")
+VERSION = "1.1.1"
+
+print(f"PyGDRPC v{VERSION} \n \t Starting...")
 try:
     memory = gd.memory.get_memory()
 except RuntimeError:
     print("Open Geometry Dash before running this!")
     os.system("PAUSE")
+
 smallimage = "none" # fallback in case of the difficulty face not being returned
 client = gd.Client() 
 scenev = memory.get_scene_value()
@@ -20,7 +22,8 @@ iseditor = memory.is_in_editor()
 name = memory.get_level_name()
 client_id = '703049428822655048'
 RPC = Presence(client_id)
-print("Connecting...")
+
+print("\t Connecting...")
 try:
     RPC.connect()
 except:
@@ -42,26 +45,27 @@ async def get_difficulty(level: gd.Level) -> str:
             base.append("featured")
         return '-'.join(base)
 
-while True:
+async def run():
     memory.reload()
-    percent = memory.get_normal_percent()
-    if percent == 0:
-        percent = "   "
-    else:
-        percent = "(" + str(percent) + "%)"
+    percent = str(memory.get_normal_percent())
     if scenev == 3 and iseditor == False and ltypev == 3:
         id = memory.get_level_id()
         smallimage = asyncio.run(get_difficulty(id))
-        RPC.update(pid=memory.process_id, state=str(f"{name} {percent}"), details="Playing a level.", large_image="gd", small_image=asyncio.run(get_difficulty(id)))
+        RPC.update(pid=memory.process_id, state=str(f"{name} ({percent}%)"), details="Playing a level", large_image="gd", small_image=asyncio.run(get_difficulty(id)))
     if scenev == 3 and iseditor and ltypev != 2:
         RPC.update(pid=memory.process_id, details="In the editor.", large_image="gd")
     if scenev == 3 and iseditor == False and ltypev == 2:
-        RPC.update(pid=memory.process_id, state=str(f"{name} {percent}"), details="Playing an editor level.", large_image="gd")
+        RPC.update(pid=memory.process_id, state=str(f"{name} ({percent}%)"), details="Playing an editor level", large_image="gd")
     else:
         if scenev != 3 and iseditor == False and ltypev != 2 and ltypev != 3 and ltypev != 1 or percent == "   " or None and scenev != 4:
-            RPC.update(pid=memory.process_id, state="     ", details="In the menu.", large_image="gd")
+            RPC.update(pid=memory.process_id, state="     ", details="In menu", large_image="gd")
         else:
             if scenev == 9 and ltypev == 1:
                 smallimage = asyncio.run(get_difficulty(id))
-                RPC.update(pid=memory.process_id, state="     ", details="Playing an official level.", large_image="gd", small_image=smallimage)
+                RPC.update(pid=memory.process_id, state="     ", details="Playing an official level", large_image="gd", small_image=smallimage)
+
+print("\n Running!")
+
+while True:
+    asyncio.run(run())
     time.sleep(5)
